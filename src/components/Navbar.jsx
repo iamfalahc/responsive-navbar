c9c65e8a03fcc3174a4b30f2bf41008f23773edf
Muhammed Falah <iamfalahc@gmail.com>
2024-05-14 14:07:18 +0530

    final push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="navbar">
-      <section className="left">
-        <h4>Brand Name</h4>
-      </section>
-      <section className="right">
-        <span>HOME</span>
-        <span>ABOUT</span>
-        <span>SERVICE</span>
-        <span>CONTACT</span>
-      </section>
-      <span><FiMenu /></span>
+      <div className="navbar-container">
+        <div className="brand">
+          <h4>Brand Name</h4>
+        </div>
+        <div className={`menu-items ${menuOpen ? "open" : ""}`}>
+          <span>HOME</span>
+          <span>ABOUT</span>
+          <span>SERVICES</span>
+          <span>CONTACT</span>
+        </div>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <FiMenu />
+        </div>
+      </div>
     </div>
   );
 };
